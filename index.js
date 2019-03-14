755575a71e762ad74d8c1542a50d8ca1676c6f78
Zach Mobilian <zach.mobilian@blackrock.com>
2019-03-14 22:10:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,5 +26,6 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else
+  var line = [];
   return(`The line is currently:` + line);
 }
