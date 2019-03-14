4e93312924013c569d72b50ca4cc980665991a30
Zach Mobilian <zach.mobilian@blackrock.com>
2019-03-14 21:50:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+var katzDeliLine = [];
 
-function takeANumber(katzDeli, name){
-  katzDeli.push(name)
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
   return 'Welcome, ' + name + '. You are number ' + katzDeli.length + ' in line.'
+}
+
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var name = katzDeliLine[0];
+    katzDeliLine.splice(0, 1);
+    return "Currently serving " + name + ".";
+  }
 }
