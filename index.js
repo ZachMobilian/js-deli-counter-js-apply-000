825b78feea54e7ed59cb63d00b6807c7d3a4c6c5
Zach Mobilian <zach.mobilian@blackrock.com>
2019-03-14 21:39:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function takeANumber(katzDeli, name){
   if (katzDeli.length >0){
     katzDeli.push(name)
   }
-  return 'Welcome, ' + name + '. You are number ' + katzDeli.length+1 + ' in line.'
+  return 'Welcome, ' + name + '. You are number ' + katzDeli.length + ' in line.'
 }
